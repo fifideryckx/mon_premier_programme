1543a5a69c20eee648ab0ac857c89bdded8a5897
Ph. Admin <CashExpress@MacBook-de-Fifi.local>
2012-08-03 15:27:28 +0200

    Initial Commit

---
+++ mon_premier_programme/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mon_premier_programme
+//
+//  Created by Ph. Admin on 03/08/12.
+//  Copyright (c) 2012 Ph. Admin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
